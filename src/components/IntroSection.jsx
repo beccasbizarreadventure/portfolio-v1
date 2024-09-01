8ec755de9483585cf9805bf87b7c8935c3fef577
Rebecca <rebecca90469@gmail.com>
2024-09-01 16:50:33 -0400

    Add intro section skeelton

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const IntroSection = () => {
   return (
-    <section id="intro" className="section">
-      <h1>About</h1>
-      <p>Information about me.</p>
+    <section id="intro" className="section h-screen ml-10 items-left justify-center">
+      <h1 className='font-header text-header_text mb-10'>
+        <span className="block relative text-6xl">REBECCA SMITH - </span>
+        <span className="block relative text-4xl ">JUNIOR FULL STACK DEVELOPER</span>
+      </h1>
+      <div className="flex flex-row">
+        <div className="bg-accent h-[40rem] w-[30rem] rounded-2xl relative before:absolute before:h-[40rem] before:w-[30rem] before:rounded-2xl before:bg-secondary before:top-2 before:left-3 before:-z-10" />
+        <p className='ml-[10rem] mt-[2rem] text-text font-body text-lg'>Information about me goes here</p>
+      </div>
     </section>
   );
 };
