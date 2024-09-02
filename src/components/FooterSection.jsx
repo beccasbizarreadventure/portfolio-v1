d0698bda40ae691ec4d3a0bb82a0bc61a160da13
Rebecca <rebecca90469@gmail.com>
2024-09-02 13:13:04 -0400

    Added odd project skeleton

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const FooterSection = () => {
+
+  const links = [
+    {
+      link: "https://github.com/beccasbizarreadventure",
+      icon: "fa-brands fa-github",
+    },
+    {
+      link: "https://www.linkedin.com/in/rebecca-s-48bb01323/",
+      icon: "fa-brands fa-linkedin",
+    },
+  ];
+
   return (
     <section
       id="contact"
@@ -11,28 +23,20 @@ const FooterSection = () => {
         STAY IN TOUCH
       </h1>
       <div className="flex space-x-10 pb-10">
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className="relative"
-        >
-          <a href="https://github.com/beccasbizarreadventure">
-            <button className="h-[4rem] w-[4rem] rounded-full text-2xl text-buttons bg-contrast relative before:absolute before:h-[4rem] before:w-[4rem] before:rounded-full before:bg-secondary before:top-0 before:left-2 before:-z-10">
-              <i className="fa-brands fa-github" />
-            </button>
-          </a>
-        </motion.div>
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className="relative"
-        >
-          <a href="https://www.linkedin.com/in/rebecca-s-48bb01323/">
-            <button className="h-[4rem] w-[4rem] rounded-full text-2xl text-buttons bg-contrast relative before:absolute before:h-[4rem] before:w-[4rem] before:rounded-full before:bg-secondary before:top-0 before:left-2 before:-z-10">
-              <i className="fa-brands fa-linkedin" />
-            </button>
-          </a>
-        </motion.div>
+        {links.map((link, index) => (
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="relative"
+            key={index}
+          >
+            <a href={link.link}>
+              <button className="h-[4rem] w-[4rem] rounded-full text-2xl text-buttons bg-contrast relative before:absolute before:h-[4rem] before:w-[4rem] before:rounded-full before:bg-secondary before:top-0 before:left-2 before:-z-10">
+                <i className={link.icon} />
+              </button>
+            </a>
+          </motion.div>
+        ))}
       </div>
     </section>
   );
--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -8,8 +8,9 @@ const IntroSection = () => {
         <p className="block relative text-4xl pb-[5rem]">JUNIOR FULL STACK DEVELOPER</p>
       </h1>
       <div className="flex flex-row">
-        <div className="bg-accent h-[40rem] w-[30rem] rounded-2xl relative before:absolute before:h-[40rem] before:w-[30rem] before:rounded-2xl before:bg-secondary before:top-2 before:left-3 before:-z-10" />
-        <p className='pl-[10rem] pt-[2rem] text-text font-body text-lg'>Information about me goes here</p>
+        <div className="bg-accent h-[40rem] w-[30rem] rounded-2xl relative before:absolute before:h-[40rem] before:w-[30rem] before:rounded-2xl before:bg-secondary before:top-2 before:left-3 before:-z-10">
+        </div>
+        <p className='pl-[10rem] pt-[2rem] text-text font-body text-lg mr-60'>Information about me goes here</p>
       </div>
     </section>
   );
